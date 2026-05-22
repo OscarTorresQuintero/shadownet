084dd6c9871fd5fab76ff16080d3ff3d89eae700
Oskitar.js <oscartquintero23@gmail.com>
2026-05-21 21:30:18 -0500

    "feat: implement terminal typewriter text animation"

--- lib/widgets/terminal_widgets.dart
+++ lib/widgets/terminal_widgets.dart
@@ -31,3 +31,58 @@ class _BlinkingCursorState extends State<BlinkingCursor> {
     );
   }
 }
+class TypewriterText extends StatefulWidget {
+  final String text;
+  final TextStyle? style;
+  final Duration speed;
+  final VoidCallback? onComplete;
+
+  const TypewriterText({
+    super.key,
+    required this.text,
+    this.style,
+    this.speed = const Duration(milliseconds: 30),
+    this.onComplete,
+  });
+
+  @override
+  State<TypewriterText> createState() => _TypewriterTextState();
+}
+
+class _TypewriterTextState extends State<TypewriterText> {
+  String _displayed = '';
+  int _index = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(widget.speed, (_) {
+      if (_index < widget.text.length) {
+        if (mounted) {
+          setState(() {
+            _displayed += widget.text[_index];
+            _index++;
+          });
+        }
+      } else {
+        _timer?.cancel();
+        widget.onComplete?.call();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _displayed,
+      style: widget.style ?? TerminalTheme.terminalText,
+    );
+  }
+}
