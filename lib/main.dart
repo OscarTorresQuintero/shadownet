3c29f4265cc13fa626f623e11ee99e807b42ae0f
Oskitar.js <oscartquintero23@gmail.com>
2026-05-21 21:56:52 -0500

    chore: initialize ShadowNet app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'theme/terminal_theme.dart';
-import 'screens/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Forzar orientación vertical
+  // Bloquea la rotación a vertical
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-
-  // Barra de estado oscura y transparente
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: TerminalTheme.bgColor,
-      systemNavigationBarIconBrightness: Brightness.light,
-    ),
-  );
 
   runApp(const ShadowNetApp());
 }
@@ -32,8 +20,6 @@ class ShadowNetApp extends StatelessWidget {
     return MaterialApp(
       title: 'ShadowNet',
       debugShowCheckedModeBanner: false,
-      theme: TerminalTheme.darkTheme,
-      home: const AuthScreen(), // Siempre inicia en biometría
     );
   }
 }
